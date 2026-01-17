e8326f120870bbc500406a98d4f55faaf7a4f4cc
Oleg Zapara <ole9zp@gmail.com>
2026-01-17 14:11:43 +0200

    a

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 from core.neo4j_driver import init_driver, close_driver
+
 load_dotenv()
 
 from repositories.ingest_repo import GraphRepository
 from agent.agent import LangGraphIngestionAgent
 
-init_driver()          # <-- важливо
+init_driver()  # what the fuck
 
 
 repo = GraphRepository()
@@ -13,7 +14,21 @@ repo.ensure_constraints()
 
 agent = LangGraphIngestionAgent(repo=repo)
 
-facts = agent.run(raw_input={"items": [{"RNOKPP": "2935512345", "last_name": "АДЕЛЬРЕЇВ", "first_name": "ЕДМУНД", "middle_name": "АДОЛЬФОВИЧ", "date_birth": "1980-05-15"}], "id": "З-2025-1833-062-u5p"}, max_fix_attempts=2)
+facts = agent.run(
+    raw_input={
+        "items": [
+            {
+                "RNOKPP": "2935512345",
+                "last_name": "АДЕЛЬРЕЇВ",
+                "first_name": "ЕДМУНД",
+                "middle_name": "АДОЛЬФОВИЧ",
+                "date_birth": "1980-05-15",
+            }
+        ],
+        "id": "З-2025-1833-062-u5p",
+    },
+    max_fix_attempts=2,
+)
 print(facts)
 close_driver()
 print("DONE ✅")
